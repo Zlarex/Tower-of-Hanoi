8b1c2ac94be576f81cc4fc84711e70c51438a4a7
dianafauziah13 <78580178+dianafauziah13@users.noreply.github.com>
2021-07-22 21:49:54 +0700

    menuPracticeGame

--- game.c
+++ game.c
@@ -61,10 +61,22 @@ void menuLoadGame(Game *game)
  * Menu untuk membuat permainan yang dikustomisasi oleh pemain
  * 
  * @author
+ * Diana Fauziah
+ * 07/21/21
  */
 void menuPracticeGame(Game *game)
 {
-
+	printf("-----------------MENU PRACTICE GAME---------------\n\n");
+	
+	while(true){
+	system("cls");
+	printf("/t/tMasukan Banyaknya disk pada permainan : ");
+	scanf("%d", &game->towerLevel);
+		if(game->towerLevel == 1 || game->towerLevel== 2 || game->towerLevel == 3 ||game->towerLevel == 4){
+			menuLobby(&game);
+			return;
+		}	
+	}
 }
 
 /**
